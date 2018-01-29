345983ee16bb95381d7e623a99760bd273a4e475
wduffell <35709418+wduffell@users.noreply.github.com>
2018-01-29 17:43:17 -0500

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,9 @@
+# Introduction to Programming
+# Author: Willow Duffell
+# Date: 1/29/2018
+
+def convert ():
+    distance = eval( input( "Enter a distance in miles:"))
+    seconds = distance / 186000
+    print ("The time as seconds is", seconds)
+    
