8d4288043c96f6269407832136044c80df755b67
wduffell <35709418+wduffell@users.noreply.github.com>
2018-01-31 20:35:33 -0500

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,12 @@
+# Introduction to Programming
+# Author: Willow Duffell
+# Date: 1/31/18
+
+noun = input ("Enter a noun:")
+verb = input ("Enter a verb:")
+adjective = input ("Enter an adjective:")
+noun2= input ("Enter another noun:")
+place = input ("Enter a place:")
+
+print ("The girl and her", noun, verb, "into the", adjective, noun2,"in", place)
+
