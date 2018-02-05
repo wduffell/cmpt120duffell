eead05c92df70351f3505eb4a559076b4de5a81d
wduffell <35709418+wduffell@users.noreply.github.com>
2018-02-05 17:47:28 -0500

    Wrote programs to compute fibonacci and pi. Resolves #1.

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+# Introduction to Programming
+# Author: Willow Duffell
+# Date: 2/5/18
+
+def fibonacci (n):
+    if n == 1 or n == 2:
+        return 1
+    return fibonacci (n-1) + fibonacci (n-2)
+
+    
+    
---
+++ pi.py
@@ -0,0 +1,20 @@
+# Introduction to Programming
+# Author: Willow Duffell
+# Date: 2/5/18
+
+def main():
+    n = int (input ("Enter the number of terms to use:"))
+    sign = 1
+    pi = 0
+    for i in range (1, n * 2 + 1, 2):
+        term = 4/i * sign
+        pi = pi + term
+        sign = sign * -1
+    print ("The approximated value of pi is:", pi)
+
+main()
+
+
+
+
+        
