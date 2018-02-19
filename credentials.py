9d77d1275aedc6fe8e0e488c61a722a6f59d5f28
wduffell <35709418+wduffell@users.noreply.github.com>
2018-02-19 16:07:35 -0500

    Enter first and last name

---
+++ credentials.py
@@ -0,0 +1,10 @@
+# CMPT 120 Intro to Programming
+# Lab #5 - Working with Strings and Functions
+# Author: Willow Duffell
+# Created: 2018-02-19
+
+def main():
+
+# get user's first and last name
+    first = input("Enter your first name: ")
+    last = input("Enter your last name: ")
