ceabd3892365e766cb597ffeaadc95451fedc508
wduffell <35709418+wduffell@users.noreply.github.com>
2018-02-27 15:48:22 -0500

    Initial version of a simple guessing game

---
+++ guessing-game.py
@@ -0,0 +1,21 @@
+# Introduction to Programming
+# Lab Assignment #5
+# Author: Willow Duffell
+# Date: 2/26
+
+
+#Define a variable to hold the name of an animal (this is the answer)
+
+def main():
+   guessedright = False
+   answer = "pig"
+   animalguess = input("I'm thinking of an animal... Try to guess it: ")
+   while guessedright == False :
+      if animalguess == answer :
+         print("Congratualtions! That's correct!")
+         guessedright = True
+      else:
+         animalguess = input("Sorry that's incorrect. Try again: ")
+   return
+
+main()
