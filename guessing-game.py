24d067e6f049861ac43673b8dc452c4f9afccc04
wduffell <35709418+wduffell@users.noreply.github.com>
2018-02-27 16:21:26 -0500

    Usability improvements to the guessing game, resolves #2 and #3

--- guessing-game.py
+++ guessing-game.py
@@ -10,12 +10,17 @@ def main():
    guessedright = False
    answer = "pig"
    animalguess = input("I'm thinking of an animal... Try to guess it: ")
+   animalguess = animalguess.lower()
    while guessedright == False :
       if animalguess == answer :
          print("Congratualtions! That's correct!")
          guessedright = True
+      elif animalguess == "quit":
+         return
       else:
          animalguess = input("Sorry that's incorrect. Try again: ")
+         animalguess = animalguess.lower()
    return
 
 main()
+
