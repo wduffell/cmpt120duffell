7ff4654dac798981b40832245e4c13d683536f12
wduffell <35709418+wduffell@users.noreply.github.com>
2018-03-09 18:06:58 -0500

    Add files via upload

---
+++ Project/calc_functions.py
@@ -0,0 +1,26 @@
+#Intro to Programming
+#Author: Willow DUffell
+#Date: 2/23/
+
+#Calculator functions
+
+
+def add2numbers(x,y):
+    return x + y
+def subtract2numbers(x,y):
+    return x - y
+def multiply2numbers(x,y):
+    return x * y
+def divide2numbers(x,y):
+    return x / y
+def changesign(x):
+    return x * -1
+def percent(x):
+    return x / 100
+def squareroot(x):
+    return x ** .5
+def square(x):
+    return x ** 2
+def inverse(x):
+    return 1 / x
+    
