3455c97db6282908a067df5697474a9d96290ff9
wduffell <35709418+wduffell@users.noreply.github.com>
2018-03-29 16:58:58 -0400

    Added more functions

--- calc_functions.py
+++ calc_functions.py
@@ -4,6 +4,7 @@
 
 #Calculator functions
 
+import math 
 
 def add2numbers(x,y):
     return x + y
@@ -23,4 +24,23 @@ def square(x):
     return x ** 2
 def inverse(x):
     return 1 / x
-    
+def xy(x,y):
+    return x ** y
+def sin(x):
+    return math.sin(x)
+def cos(x):
+    return math.cos(x)
+def tan(x):
+    return math.tan(x)
+def tenx(x):
+    return 10 ** x
+def log(x):
+    return math.log10(x)
+def arcsin(x):
+    return math.asin(x)
+def arccos(x):
+    return math.acos(x)
+def arctan(x):
+    return math.atan(x)
+def ln(x):
+    return math.log(x)
