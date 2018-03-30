47d4e46ff1ccfa03f9ce82d7bb82176e21cdc717
wduffell <35709418+wduffell@users.noreply.github.com>
2018-03-30 19:01:10 -0400

    Fixed board

--- tictactoe.py
+++ tictactoe.py
@@ -9,11 +9,11 @@ symbol = [ " ", "x", "o" ]
    
 def printBoard(board):
     print("+-----------+")
-    print("|   |   |   |")
+    print("| ",board[6],"| ",board[7],"| ",board[8],"|")
     print("+-----------+")
-    print("|   |   |   |")
+    print("| ",board[3],"| ",board[4],"| ",board[5],"|")
     print("+-----------+")
-    print("|   |   |   |")
+    print("| ",board[0],"| ",board[1],"| ",board[2],"|")
     print("+-----------+")
 
 printBoard(['','','','','','','','',''])
