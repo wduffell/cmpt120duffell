83074cc4f45f7ef84680a75135d1c286da7b1866
wduffell <35709418+wduffell@users.noreply.github.com>
2018-04-10 15:53:35 -0400

    look up new emotion and display emotion

--- personality.py
+++ personality.py
@@ -16,54 +16,59 @@
 #1 = "punish"
 #2 = "threaten"
 #3 = "joke"
+
 import random
+
+emotionMatrix = [
+    [4, 0, 0, 2],
+    [2, 0, 0, 2], 
+    [2, 3, 5, 2],
+    [2, 3, 3, 2],
+    [2, 3, 5, 2],
+    [4, 3, 5, 2]
+]
+
 feelingslist = ["anger", "disgust", "happiness", "sadness", "surprise", "fear"]
 actionslist = ["reward", "punish", "threaten", "joke"]
-#Make a table
-def introduction():
-    print("Hi I'm Al")
-    print("Today I'm feeling:" + random.choice(feelingslist))
-# Get the mode of interaction from the user
-# Params: none
-# Returns: an integer indicating one of reward, punish, joke, or threaten
-def getInteraction():
-    action = input("What do you want to do to Al? (Type either reward, punish, joke or threaten): ")
-    if action == "reward":
-        return 0
-    elif action == "punish":
-        return 1
-    elif action == "threaten":
-        return 2
-    elif action == "joke":
-        return 3
-    else:
-        return 4
-    
-        
-    
-# Based on a given emotion and action, determine the next emotional state
-# Params:
-# currEmotion - a current emotion
-# userAction - a user interaction
-# Returns: an emotion
-def lookupEmotion(currEmotion, userAction):
-#pass # TODO do the matrix lookup
-    return 0 # return an integer corresponding to an emotion
+reactionlist = ["Grrr! That made me angry!", "Yuck! that is disgusting", "Wooohooo! that made me happy", "Boohoo, you are going to make me cry", "Wow! Very Surprising",  "Now I'm scared"]
 
+
+def introduction():
+    print("Hi I'm AI, my name is AL")
+    emotion = random.choice(feelingslist)
+    indexfeeling = feelingslist.index(emotion)
+    print("Today I'm feeling: " + emotion)
+    return indexfeeling
+
+def getInteraction():
+    userAction = input("What do you want to do to Al? (Type either reward, punish, joke or threaten): ")
+    try:
+        userAction = actionslist.index(userAction)
+    except:
+        userAction = 4
+    return userAction
+    
+    
+def lookupEmotion(currEmotion, userAction):
+
+    newemotion = emotionMatrix[currEmotion][userAction]
+    return newemotion
 
 def main():
     goonforever = True
-    introduction()
+    currEmotion = introduction()
     while goonforever == True:
-        returnedaction = getInteraction()
-        if returnedaction == 4:
+        userAction = getInteraction()
+        if userAction == 4:
             print("Invalid Action")
+            print('\n')
             continue
         else:
-            print("Action = " + actionslist[returnedaction])
+            currEmotion = lookupEmotion(currEmotion, userAction)
+            print(reactionlist[currEmotion])
+            print('\n')
     return
 
 main()
-    
 
     
