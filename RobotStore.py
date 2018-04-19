978b08202e37722f5f7d08a889163d31debfbddd
wduffell <35709418+wduffell@users.noreply.github.com>
2018-04-19 13:19:17 -0400

    added methods to my product class

--- RobotStore.py
+++ RobotStore.py
@@ -3,22 +3,34 @@
 # Date: 4/21/18
 # Lab 9
 
-class Product:
-    def __init___ (self, name, price, quantity): #constructor #self access attributes
-        self.name = name
-        self.price = price
-        self.quantity = quantity #assign values
-        
-        productNames = [ "Ultrasonic range finder"
+productNames = [ "Ultrasonic range finder"
                         , "Servo motor"
                         , "Servo controller"
                         , "Microcontroller Board"
                         , "Laser range finder"
                         , "Lithium polymer battery"
                         ]
-        productPrices = [ 2.50, 14.99, 44.95, 34.95, 149.99, 8.99 ]
-        productQuantities = [ 4, 10, 5, 7, 2, 8 ]
-    
+productPrices = [ 2.50, 14.99, 44.95, 34.95, 149.99, 8.99 ]
+productQuantities = [ 4, 10, 5, 7, 2, 8 ]
+
+class Product:
+    def __init___ (self, name, price, quantity): #constructor #self access attributes
+        self.name = name
+        self.price = price
+        self.quantity = quantity #assign values
+        
+    def isinstock(self, count):
+        if self.quantity >= count:
+            return True
+        else:
+            return False
+
+    def totalCost(self, count):
+        return float(self.price) * int(count)
+
+    def productRemove(self, count, prodId):
+        productQuantities[prodId] -= count
+        return 
 
 def printStock():
     print()
@@ -31,17 +43,23 @@ def printStock():
 
 def main():
     cash = float(input("How much money do you have? $"))
+    
     while cash > 0:
         printStock()
         
         vals = input("Enter product ID and quantity you wish to buy: ").split(" ")
-        
         if vals[0] == "quit": break
         
         prodId = int(vals[0])
         count = int(vals[1])
-        
-        if productQuantities[prodId] >= count:
+
+        myProduct= Product()
+        myProduct.name = productNames[prodId]
+        myProduct.price = productPrices[prodId]
+        myProduct.quantity = productQuantities[prodId]       
+
+        instock = myProduct.isinstock(count)  
+        if instock == True:
             if cash >= productPrices[prodId]:
                 productQuantities[prodId] -= count
                 cash -= productPrices[prodId] * count
