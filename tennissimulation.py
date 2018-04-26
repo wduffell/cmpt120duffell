3c7c798e08bb848299dc00a58277524a42cbb255
wduffell <35709418+wduffell@users.noreply.github.com>
2018-04-26 15:58:16 -0400

    created file and imported simstats and tennis match

---
+++ tennissimulation.py
@@ -0,0 +1,25 @@
+#tennissimulation.py
+
+from simstats import SimStats
+from tennismatch import TennisMatch
+
+def main():
+    printIntro()
+    probA, probB, n = getInputs()
+    winsA, winsB = simNGames(n, probA, probB)
+    printSummary(winsA, winsB)
+
+def printIntro():
+    print("This program simulates a game of racquetball between two")
+    print('players called "A" and "B". The abilities of each player is')
+    print("indicated by a probability (a number between 0 and 1) that")
+    print("the player wins the popint when serving. Player A always")
+    print("has the first serve.\n")
+
+def getInputs():
+    a = float(input("What is the prob. player A wins a serve? "))
+    b = float(input("What is the prob. player B wins a serve? "))
+    n = int(input("How many games to simulate? "))
+    return a, b, n
+
+main()
