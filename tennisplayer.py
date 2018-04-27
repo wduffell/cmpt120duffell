044866911e30971f9f3de6a39378fb290216d6c9
wduffell <35709418+wduffell@users.noreply.github.com>
2018-04-26 20:57:27 -0400

    created player class for tennis game

--- tennisplayer.py
+++ tennisplayer.py
@@ -6,10 +6,15 @@ class Player:
     def __init__(self, prob):
         self.prob = prob
         self.score = 0
+        self.sets = 0
+        self.games = 0
 
-    def winsServe(self):
+    def scorePoint(self):
         # Returns true with probability self.prob
         return random() <= self.prob
+    
+    def getSets(self):
+        return self.sets
 
     def incScore(self):
         # Add a point to this player's score
@@ -19,9 +24,25 @@ class Player:
             self.score = 30
         elif self.score == 30:
             self.score = 40
+
+    def grabScore(self):
+        #Return the player's current score
+        return self.score
+
+    def gameVictory(self):
+        self.games = self.games + 1
+
+    def setVictory(self):
+        self.advantage = True
+
+    def noAdvantage(self):
+        self.advantage = False
+
+    def advantage(self):
+        self.advantage = False
+
+    
         
             
 
-    def getScore(self):
-        # Return this player's current score
-        return self.score
+
